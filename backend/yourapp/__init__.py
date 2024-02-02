71f309597877c843862f9cfe80086faeb55fad6a
Bonmu Ku <bonmurandku@berkeley.edu>
2024-02-02 15:31:57 +0900

    restructured and functionality restored

--- backend/yourapp/__init__.py
+++ backend/yourapp/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from flask_cors import CORS
 
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder='../templates')  # Adjust the path to the templates folder
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../summarizer.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG'] = True
+CORS(app)
 
 
 db = SQLAlchemy(app)
--- backend/yourapp/routes.py
+++ backend/yourapp/routes.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify, send_from_directory
+from flask import request, jsonify, send_from_directory, render_template
 from yourapp import app, db
 from yourapp.models import TextRecord
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
@@ -11,11 +11,19 @@ def index():
     FRONTEND_FOLDER = os.path.abspath("../../frontend")
     return send_from_directory(FRONTEND_FOLDER, 'index.html')
 '''
+
+'''
+2nd version
 @app.route('/')
 def index():
     # Navigate up one directory from the 'yourapp' package to the 'backend' directory
     backend_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     return send_from_directory(backend_dir, 'index.html')
+'''
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 # Serve static files (css, js, images, etc.)
 @app.route('/<path:path>')
@@ -24,22 +32,28 @@ def static_proxy(path):
     return send_from_directory(FRONTEND_FOLDER, path)
 
 # Summarization route
+
 @app.route('/summarize', methods=['POST'])
 def summarize():
+    # Extract the text to be summarized from the incoming JSON request
     data = request.get_json()
     text = data['text']
 
+    # Initialize the tokenizer and model for Pegasus
     tokenizer = PegasusTokenizer.from_pretrained('tuner007/pegasus_summarizer')
     model = PegasusForConditionalGeneration.from_pretrained('tuner007/pegasus_summarizer')
 
+    # Tokenize the input text and generate the summary
     tokens = tokenizer(text, truncation=True, padding='longest', return_tensors='pt')
     summary_ids = model.generate(tokens['input_ids'], num_beams=4, max_length=60, min_length=20, length_penalty=2.0, early_stopping=True)
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
-    record = TextRecord(input_text=text, output_text=summary)  # Ensure you use 'text' from 'data' for 'input_text'
+    # Save the input text and its summary to the database
+    record = TextRecord(input_text=text, output_text=summary)
     db.session.add(record)
     db.session.commit()
 
+    # Return the summary in the response
     return jsonify({'summary': summary})
 
 # Route to fetch all records
